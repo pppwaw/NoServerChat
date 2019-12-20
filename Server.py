1c7c31040a98f48cbeeb900564938f9c64cb750e
pppwaw <2504454577@qq.com>
2019-12-20 18:25:41 +0800

    update

--- Server.py
+++ Server.py
@@ -4,11 +4,41 @@ from hypercorn import Config
 from hypercorn.asyncio import serve
 
 app = Quart(__name__)
+up = {"asd": "123"}
+
+
+def rtn(code: int, message: str):
+    return json.dumps({"code": code, "message": message})
+
+
+async def auth(strjson) -> tuple:
+    try:
+        d = json.loads(strjson)
+    except:
+        return (False, "")
+    else:
+        if "username" in d and "password" in d:
+            if up[d["username"]] == d["password"]:
+                return True, d["username"]
 
 
 @app.websocket("/client")
-def client():
-    pass  # websocket
+async def client():
+    while True:
+        r = await websocket.receive()
+        rt = await auth(r)
+        if rt[0]:
+            print(rt[1], "Login!")
+            while True:
+                r = await websocket.receive()
+                try:
+                    dr = json.loads(r)
+                except:
+                    await websocket.send("NoJSON")
+                else:
+                    await websocket.send(json.dumps(dr))
+        else:
+            await websocket.send(rtn(1, "Invalid username or password"))
 
 
 if __name__ == '__main__':
@@ -19,5 +49,6 @@ if __name__ == '__main__':
     config.bind = ["0.0.0.0:5700", ":::5700"]
     config.access_logger = logger
     config.error_logger = logger
+    config.use_reloader = True
     asyncio.run(serve(app=app, config=config))
     # app.run(host="0.0.0.0",port=5699,use_reloader=True)
